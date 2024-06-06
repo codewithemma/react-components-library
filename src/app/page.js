1026a43b7cdc785488656a79e8a7381d24090307
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-06-06 14:57:47 +0100

    code refactor

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,9 @@ import styles from "./page.module.css";
 import Invoice from "@/components/invoice/Invoice";
 
 export default function Home() {
+  {
+    /* import component to test in browser */
+  }
   return (
     <main className={styles.main}>
       <Invoice />
