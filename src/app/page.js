7a6d1bfaf7292856439b24837745552a24d2292a
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-07-10 19:32:59 +0100

    push

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,11 @@
-// import DeviceFetch from "@/components/deviceFetch/DeviceFetch";
-import App from "@/components/select/Select";
-import styles from "./page.module.css";
-import Dummy from "@/components/dummy/Dummy";
 import DeviceFetch from "@/components/deviceFetch/DeviceFetch";
-// import Invoice from "@/components/invoice/Invoice";
 
 export default function Home() {
   {
     /* import component to test in browser */
   }
   return (
-    <main className={styles.main}>
+    <main>
       {/* added a new device component */}
       <DeviceFetch />
       {/* this is an example */}
